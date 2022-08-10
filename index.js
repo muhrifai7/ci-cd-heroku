5404f4b9bf4d3e90928b10af0c1917e486dd1188
muh rifai <muhrifai554@gmail.com>
2022-08-10 13:56:21 +0700

    add testing

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const express = require("express");
-const app = express();
-const port = process.env.port || 8100;
+// const express = require("express");
+// const app = express();
+// const port = process.env.port || 8100;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.status(200).send("Hello World!");
+// });
 
-app.get("/users", (req, res) => {
-  res.status(200).send([]);
-});
+// app.get("/users", (req, res) => {
+//   res.status(200).send([]);
+// });
 
-app.listen(port, () => {
-  console.log("Our App Is Up And Running");
-});
+// app.listen(port, () => {
+//   console.log("Our App Is Up And Running");
+// });
 
+const app = {
+  sum: function (a, b) {
+    return a + b;
+  },
+};
 module.exports = app;
--- index.test.js
+++ index.test.js
@@ -2,21 +2,24 @@ const request = require("supertest");
 const app = require(".");
 
 describe("Test the root path", () => {
-  test("It should response the GET method", async () => {
-    const res = await request(app).get("/");
-    expect(res.statusCode).toBe(200);
-  });
+  // test("It should response the GET method", () => {
+  // const res = await request(app).get("/");
+  // expect(res.statusCode).toBe(200);
+  // });
 
-  test("It should response the text Hello World!", () => {
-    request(app)
-      .get("/")
-      .then((response) => {
-        expect(response.text).toBe("Hello World!");
-      });
-  });
+  // test("It should response the text Hello World!", () => {
+  //   request(app)
+  //     .get("/")
+  //     .then((response) => {
+  //       expect(response.text).toBe("Hello World!");
+  //     });
+  // });
 
-  test("it should return empty array", async () => {
-    const users = await request(app).get("/users");
-    expect(users.req.outputData).toEqual([]);
+  // test("it should return empty array", () => {
+  //   const users = request(app).get("/users");
+  //   expect(users.req.outputData).toEqual([]);
+  // });
+  test("should return sum", () => {
+    expect(app.sum(1, 2)).toBe(3);
   });
 });
